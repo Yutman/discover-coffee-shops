83e1c21e7dde4dffe032d3f278e8c7e3d074524f
Peter Kuria <kuriapeter95@gmail.com>
2025-01-17 13:10:43 +0300

    Add banner component and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import Banner from "@/components/banner.client";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center 
-          sm:items-start">
-        <h1>Coffee Connoisseur</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-14">
+       <Banner/> 
     </main>
   );
 }
