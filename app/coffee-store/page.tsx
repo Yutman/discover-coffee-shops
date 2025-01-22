726a60cbf9a5522164a16927a54663ccbe6490fa
Peter Kuria <kuriapeter95@gmail.com>
2025-01-22 10:48:52 +0300

    Creating nested routes link

--- app/coffee-store/page.tsx
+++ app/coffee-store/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export default function Page() {
-  return <div>
-      Coffee store page
-    </div>
+  return <div>Coffee store Page</div>;
 }
