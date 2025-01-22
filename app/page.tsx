f28cda2a0e436b5ce5212d989b1e1ca0114b4a15
Peter Kuria <kuriapeter95@gmail.com>
2025-01-22 10:54:44 +0300

    Implement functionality to route back and forth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Banner from "@/components/banner.client";
+import Link from "next/link";
 
 export default function Home() {
+  const coffeeStoreId = 'dark-horse-coffee';
   return (
     <div className='mb-56'>
        <main className="flex min-h-screen flex-col items-center justify-between p-14">
-           <Banner/> 
+           <Banner/>
+           <Link href={`/coffee-store/${coffeeStoreId}`}>Dark Horse Coffee</Link> 
        </main>
     </div>
   );
