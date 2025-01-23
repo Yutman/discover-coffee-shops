96be3e3bd6077f032ca4c7443751655fbd7f679e
Peter Kuria <kuriapeter95@gmail.com>
2025-01-23 14:42:28 +0300

    Add coffeeStores array with direct Unsplash image URLs and configure next/image to allow Unsplash domain

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,47 @@ import Card from "@/components/card.server";
 
 export default function Home() {
   const coffeeStoreId = 'dark-horse-coffee';
+ const coffeeStores = [
+  {
+    name: "StrangeLove Coffee",
+    imgUrl: "https://images.unsplash.com/photo-1519337265831-281ec6cc8514?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  },
+  {
+    name: "Dark Horse Coffee",
+    imgUrl: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  },
+  {
+    name: "StrangeLove Coffee",
+    imgUrl: "https://images.unsplash.com/photo-1519337265831-281ec6cc8514?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  },
+  {
+    name: "Dark Horse Coffee",
+    imgUrl: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  },
+  {
+    name: "StrangeLove Coffee",
+    imgUrl: "https://images.unsplash.com/photo-1519337265831-281ec6cc8514?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  },
+  {
+    name: "Dark Horse Coffee",
+    imgUrl: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400",
+  },
+];
+
   return (
     <div className='mb-56'>
        <main className="mx-auto mt-10 max-w-6xl px-4">
           <Banner/>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 
           lg:gap-6">
+            {coffeeStores.map((coffeeStore, idx) => (
           <Card 
-            name='Dark Horse Coffee' 
-            imgUrl='/static/hero-image.png' 
-            href={`/coffee-store/${coffeeStoreId}`}
+            key={`${coffeeStore.name}-${idx}`}
+            name={coffeeStore.name}
+            imgUrl={coffeeStore.imgUrl} 
+            href={`/coffee-store/${idx}`}
           />
+            ))}
           </div>
        </main>
     </div>
