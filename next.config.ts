17488c2ca74ad1a664134366fa0f3e476db50626
Peter Kuria <kuriapeter95@gmail.com>
2025-02-12 12:51:53 +0300

    -Integrated Google Places API photo references for coffee store images -Updated `next.config.js` to accept images from external sources like `maps.googleapis.com`

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'], // Allow Unsplash's image CDN
+    domains: ['images.unsplash.com',  'maps.googleapis.com'], // Allow Unsplash's image CDN
   },
 };
 
