65f31dd2c090e461f965e6263b8a7c851675f832
Peter Kuria <kuriapeter95@gmail.com>
2025-02-12 12:50:24 +0300

    Replaced Mapbox API integration with Google Places API in the `fetchCoffeeStores` and `fetchCoffeeStore` functions.

--- app/coffee-store/[id]/page.tsx
+++ app/coffee-store/[id]/page.tsx
@@ -1,19 +1,52 @@
 import React from 'react'
 import Link from 'next/link';
+import { fetchCoffeeStore } from '@/lib/coffee-stores';
+import Image from 'next/image';
 
-export default function Page(props: {params: {id:string}}) {
-    const {
-        params: { id },
-      } = props;
-      console.log('id', id);
-      return (
-      <div className='h-full pb-80'> 
-        Coffee Store Page: {id}
-        <div className='m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4'>
-        <div className='mb-2 mt-24 text-lg font-bold'>
-        <Link href='/'>←Back to Home</Link>
+async function getData(id: string) {
+  return await fetchCoffeeStore(id);
+}
+
+export default async function Page(props: { params: { id: string } }) {
+  const { params: { id } } = props;
+
+  const coffeeStore = await getData(id);
+
+  console.log({ coffeeStore });
+
+  return (
+    <div className="h-full pb-80">
+      <div className="m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4">
+        <div>
+          <div className="mb-2 mt-24 text-lg font-bold">
+            <Link href="/">← Back to home</Link>
+          </div>
+          <div className="my-4">
+            <h1 className="text-4xl">{coffeeStore.name}</h1>
+          </div>
+          <Image
+            src={coffeeStore.imgUrl || 'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'}
+            width={740}
+            height={360}
+            className="max-h-[420px] min-w-full max-w-full rounded-lg border-2 sepia lg:max-w-[470px]"
+            alt={'Coffee Store Image'}
+          />
         </div>
+
+        <div className={`glass mt-12 flex-col rounded-lg p-4 lg:mt-48`}>
+          {coffeeStore.address && (
+            <div className="mb-4 flex">
+              <Image
+                src="/static/icons/places.svg"
+                width="24"
+                height="24"
+                alt="places icon"
+              />
+              <p className="pl-2">{coffeeStore.address}</p>
+            </div>
+          )}
         </div>
       </div>
-    );
+    </div>
+  );
 }
