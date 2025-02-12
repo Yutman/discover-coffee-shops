6d94e5312bc9746e18bb86d52b1333668c44f492
Peter Kuria <kuriapeter95@gmail.com>
2025-02-12 12:55:07 +0300

    Refactor code to switch from Mapbox to Google Places API for fetching coffee shops

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,37 @@
+// app/page.tsx
+
 import Banner from "@/components/banner.client";
 import Card from "@/components/card.server";
 import { fetchCoffeeStores } from "@/lib/coffee-stores";
 import { CoffeeStoreType } from "@/types";
 
-
 async function getData() {
-  //mapbox api
-    return await fetchCoffeeStores();
-  }
+  return await fetchCoffeeStores();
+}
 
 export default async function Home() {
   const coffeeStores = await getData();
 
   return (
-    <div className='mb-56'>
-       <main className="mx-auto mt-10 max-w-6xl px-4">
-          <Banner/>
-          <div className="nt-20">
-            <h2 className="nt-8 pb-8 text-4xl font-bold text-white">
-              Toronto Stores
-            </h2>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 
-          lg:gap-6">
+    <div className="mb-56">
+      <main className="mx-auto mt-10 max-w-6xl px-4">
+        <Banner />
+        <div className="nt-20">
+          <h2 className="nt-8 pb-8 text-4xl font-bold text-white">
+            Toronto Stores
+          </h2>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-6">
             {coffeeStores.map((coffeeStore: CoffeeStoreType, idx: number) => (
-          <Card 
-            key={`${coffeeStore.name}-${idx}`}
-            name={coffeeStore.name}
-            imgUrl={coffeeStore.imgUrl} 
-            href={`/coffee-store/${idx}`}
-          />
+              <Card
+                key={`${coffeeStore.name}-${idx}`}
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}`}
+              />
             ))}
           </div>
-          </div>
-       </main>
+        </div>
+      </main>
     </div>
   );
 }
